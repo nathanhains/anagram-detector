7fbd18957cec371af4b2cfecff1ee64225b248a3
nathan hains <nathanhains14@yahoo.com>
2021-02-09 15:13:41 -0500

    commit tree 19a082d1c93c769cb9e4d07e24efd50d0f1fa105

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+
+    def match(args)
+        new_array = []
+       args.collect do |arg| 
+        new_arg = arg.split("")
+        new_name =  name.split("")
+        if new_name.sort == new_arg.sort
+            new_array << arg
+        end
+    end
+    new_array
+    end
+end
